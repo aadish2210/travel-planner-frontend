f86f99829c5ab0b164a15f4ec0110a632c1163c3
aadish2210 <a.sheth2210@gmail.com>
2024-04-08 00:41:08 +0530

    authentication module integrated

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,36 +1,69 @@
 import React from 'react'
 import {Input , Button} from "@nextui-org/react";
+import axios from 'axios';
 const Authenticate = () => {
  const [name,setName] = React.useState("");
  const [password ,  setPassword] = React.useState("");
  const [email , setEmail] = React.useState("");
-  const [buttonText , setButtonText] = React.useState("Login"); 
+ const [buttonText , setButtonText] = React.useState("Login"); 
+ const handleSubmit = () =>{
+    if(buttonText=="Sign up"){
+     axios.post('http://localhost:3000/signup', {
+    "name": name,
+    "email": email,
+    "password" : password
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+   .catch(function (error) {
+    console.log(error);
+    });
+    }
+    else{
+      if(buttonText=="Sign up"){
+        axios.post('http://localhost:3000/login', {
+       "email": email,
+       "password" : password
+       })
+       .then(function (response) {
+        if(response.success) {
+          console.log("Logged in");
+        }
+       })
+      .catch(function (error) {
+       console.log(error);
+       });
+       }
+    }
+ }
+
   return (
     <div>
         <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     
-        <div className="flex w-auto  justify-center bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex w-auto  justify-center  bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   {buttonText} to your account
               </h1>
-              <form className="space-y-4 md:space-y-6">
+              <form className="space-y-4 md:space-y-6 flex flex-col" onSubmit={(e) => e.preventDefault()}>
                   {buttonText=="Sign up" && <div>
-                      <Input type="text" label="Username"/>
+                      <Input type="text" label="Username" onValueChange={setName}/>
                   </div>}
                   <div>
-                  <Input type="email" label="Email"/>
+                  <Input type="email" label="Email" onValueChange={setEmail}/>
                   </div>
                   <div>
-                    <Input type="password" label="Password"/>
+                    <Input type="password" label="Password" onValueChange={setPassword}/>
                   </div>
                   <div className="flex items-center justify-between">
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <Button className='w-auto'>{buttonText}</Button>
+                  <Button className='w-auto'variant='ghost' onClick={handleSubmit}>{buttonText}</Button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={console.log("clecikef")}>{buttonText=="Login" ? "Sign up" : "Login"}</a>
+                      Don’t have an account yet? <button onClick={()=>setButtonText(buttonText=="Login" ? "Sign up" : "Login")}>{buttonText=="Login" ? "Sign up" : "Login"}</button>
                   </p>
               </form>
           </div>
